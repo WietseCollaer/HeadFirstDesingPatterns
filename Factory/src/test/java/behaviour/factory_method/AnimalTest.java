4ddebbdb430635373e5e6ce52b3d00d3bca440b7
alexandre.vettas <alexandre.vettas@famifed.be>
2019-06-27 14:54:24 +0200

    change names in test

--- Factory/src/test/java/behaviour/factory_method/AnimalTest.java
+++ Factory/src/test/java/behaviour/factory_method/AnimalTest.java
@@ -6,18 +6,18 @@ import static org.junit.Assert.assertEquals;
 
 public class AnimalTest {
 
-    public static final String SILVERBACK_BEHAVIOUR = "Assert dominance";
+    public static final String DOMINANT_BEHAVIOUR = "Assert dominance";
     public static final String MONKEY_BEHAVIOUR = "Throw feces";
 
     @Test
     public void animal_silverback() {
         Animal silverback = new Silverback();
         silverback.beAlive();
-        assertEquals(silverback.getObservedBehaviour(), SILVERBACK_BEHAVIOUR);
+        assertEquals(silverback.getObservedBehaviour(), DOMINANT_BEHAVIOUR);
     }
 
     @Test
-    public void animal_monkey() {
+    public void animal_chimp() {
         Animal chimp = new Chimpanzee();
         chimp.beAlive();
         assertEquals(chimp.getObservedBehaviour(), MONKEY_BEHAVIOUR);
